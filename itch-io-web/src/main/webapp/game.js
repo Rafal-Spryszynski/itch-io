fae996322faf72709ce25cfd0e82af19725e489e
Rafał Spryszyński <rafal.spryszynski.praca@gmail.com>
2020-09-13 13:34:36 +0200

    Make all fields interactive and resizable.

--- itch-io-web/src/main/webapp/game.js
+++ itch-io-web/src/main/webapp/game.js
@@ -22,7 +22,6 @@ class GameScene extends Phaser.Scene {
 
         this.oneThird = .33;
         this.primaryColor = 0xffffff;
-        this.alphaInvisible = 0;
     }
 
     preload() {
@@ -41,18 +40,33 @@ class GameScene extends Phaser.Scene {
         // this.placeCrossSound.play();
         // this.placeCircleSound.play();
 
-        $(window).resize(() => {
-            this.graphics.clear();
+        $(window).resize(() => this.drawGameAfterResize());
+    }
 
-            this.drawGame();
-        });
+    drawGameAfterResize() {
+        this.graphics.clear();
+        this.destroyFields();
+        this.drawGame();
+    }
+
+    destroyFields() {
+        this.field00.destroy();
+        this.field10.destroy();
+        this.field20.destroy();
+        this.field01.destroy();
+        this.field11.destroy();
+        this.field21.destroy();
+        this.field02.destroy();
+        this.field12.destroy();
+        this.field22.destroy();
     }
 
     drawGame() {
         this.captureWindowSize();
         this.computeSizes();
         this.setLineStyle();
-        
+
+        this.createFields();
         this.drawBoard();
     }
 
@@ -71,6 +85,43 @@ class GameScene extends Phaser.Scene {
         this.graphics.lineStyle(this.boardLineWidth, this.primaryColor);
     }
 
+    createFields() {
+        this.field00 = this.add.graphics();
+        this.field10 = this.add.graphics();
+        this.field20 = this.add.graphics();
+        this.field01 = this.add.graphics();
+        this.field11 = this.add.graphics();
+        this.field21 = this.add.graphics();
+        this.field02 = this.add.graphics();
+        this.field12 = this.add.graphics();
+        this.field22 = this.add.graphics();
+
+        const firstX = 0;
+        const firstY = 0;
+        const secondX = this.fieldWidth;
+        const secondY = this.fieldHeight;
+        const thirdX = this.fieldWidth * 2;
+        const thirdY = this.fieldHeight * 2;
+        this.setFieldPosition(this.field00, firstX, firstY);
+        this.setFieldPosition(this.field10, secondX, firstY);
+        this.setFieldPosition(this.field20, thirdX, firstY);
+        this.setFieldPosition(this.field01, firstX, secondY);
+        this.setFieldPosition(this.field11, secondX, secondY);
+        this.setFieldPosition(this.field21, thirdX, secondY);
+        this.setFieldPosition(this.field02, firstX, thirdY);
+        this.setFieldPosition(this.field12, secondX, thirdY);
+        this.setFieldPosition(this.field22, thirdX, thirdY);
+    }
+
+    /**
+     * @param {Phaser.GameObjects.Graphics} fieldGraphics
+     * @param {Number} x
+     * @param {Number} y
+     */
+    setFieldPosition(fieldGraphics, x, y) {
+        fieldGraphics.setData({ x: x, y: y });
+    }
+
     drawBoard() {
         this.graphics.save();
         this.drawVerticalLine();
@@ -82,14 +133,17 @@ class GameScene extends Phaser.Scene {
         this.drawHorizontalLine();
         this.graphics.restore();
 
-        this.add.rectangle(0, 0, this.fieldWidth, this.fieldHeight, this.primaryColor, this.alphaInvisible)
-            .setOrigin(0, 0)
-            .setInteractive()
-            .on(Phaser.Input.Events.POINTER_UP, () => {
-                this.drawCross();
-            });
+        this.setInteractiveField(this.field00);
+        this.setInteractiveField(this.field10);
+        this.setInteractiveField(this.field20);
+        this.setInteractiveField(this.field01);
+        this.setInteractiveField(this.field11);
+        this.setInteractiveField(this.field21);
+        this.setInteractiveField(this.field02);
+        this.setInteractiveField(this.field12);
+        this.setInteractiveField(this.field22);
     }
-    
+
     drawVerticalLine() {
         this.graphics.translateCanvas(this.fieldWidth, 0);
         this.graphics.lineBetween(0, 0, 0, this.windowHeight);
@@ -98,6 +152,32 @@ class GameScene extends Phaser.Scene {
     drawHorizontalLine() {
         this.graphics.translateCanvas(0, this.fieldHeight);
         this.graphics.lineBetween(0, 0, this.windowWidth, 0);
+    }
+    
+    /** @param {Phaser.GameObjects.Graphics} fieldGraphics */
+    setInteractiveField(fieldGraphics) {
+        /** @type {Number} */
+        const x = fieldGraphics.data.values.x;
+        /** @type {Number} */
+        const y = fieldGraphics.data.values.y;
+
+        const field = new Phaser.Geom.Rectangle(x, y, this.fieldWidth, this.fieldHeight);
+        fieldGraphics
+            .setInteractive(field, Phaser.Geom.Rectangle.Contains)
+            .once(Phaser.Input.Events.POINTER_UP, () => this.drawCrossAtPosition(x, y));
+    }
+
+    /**
+     * @param {Number} x
+     * @param {Number} y
+     */
+    drawCrossAtPosition(x, y) {
+        this.graphics.save();
+        this.graphics.translateCanvas(x, y);
+
+        this.drawCross();
+
+        this.graphics.restore();
     }
 
     drawCross() {
